cd65e94e66f812d9c2c2886699f8f2250db53bfb
Milan-Lunagariya <128218648+Milan-Lunagariya@users.noreply.github.com>
2023-07-06 12:42:19 +0530

    Add files via upload
    Signed-off-by: Milan-Lunagariya <128218648+Milan-Lunagariya@users.noreply.github.com>

---
+++ insert.php
@@ -0,0 +1,16 @@
+<?php 
+    $con = mysqli_connect("localhost","root","","project-2");
+
+    
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email'];
+    $phone1 = $_REQUEST['phone1'];
+    $enquiry = $_REQUEST['enquiry'];
+    $chk = $_REQUEST['chk'];
+    
+    $ins = "INSERT INTO `contact` (`name`,`email`,`phone1`,`enquiry`,`chk`) VALUES ('$name','$email','$phone1','$enquiry','$chk')";
+    $res = mysqli_query($con,$ins);
+    header("location:thank.html");
+
+    
+?>
